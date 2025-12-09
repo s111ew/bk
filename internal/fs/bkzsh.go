57cd718dc66396691c48c59c422007f5289c5d09
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2025-12-09 19:12:58 +0000

    feat: add '~' to alias results in autocomplete to differentiate between relative directories

--- internal/fs/bkzsh.go
+++ internal/fs/bkzsh.go
@@ -14,38 +14,25 @@ bk_cd() {
 }
 alias cd=bk_cd
 
-source ~/.bk
-
-bk_cd() {
-    local resolved
-    resolved=$(bk --resolve "$1" 2>/dev/null)
-    if [[ -n "$resolved" ]]; then
-        builtin cd "$resolved"
-    else
-        builtin cd "$1"
-    fi
-}
-alias cd=bk_cd
-
 _bk_cd_complete() {
-    local -a bk_keys
-    local expl
+    local -a bk_keys bk_display
 
-    # Load alias keys from ~/.bk
     if [[ -f "$HOME/.bk" ]]; then
-        bk_keys=("${(f)$(awk -F '=' 'NF>=2 && $1 !~ /^[[:space:]]*#/ {print $1}' "$HOME/.bk")}")
+        bk_keys=("${(f)$(awk -F '=' '
+            NF >= 2 && $1 !~ /^[[:space:]]*#/ {print $1}
+        ' "$HOME/.bk")}")
     fi
 
-    # Use zsh native _arguments
-    # - First argument: complete either a directory or a bk alias
-    _arguments \
-        '1:directory:_files -/' \
-        && compadd -a bk_keys
+    bk_display=("${(@)bk_keys/#/~}")
+
+    _arguments '1:directory:_files -/'
+
+    if (( ${#bk_keys[@]} )); then
+        _describe -t bookmarks bookmarks bk_display bk_keys
+    fi
 }
 
-# Register completion for both cd and bk_cd
 compdef _bk_cd_complete cd bk_cd
-
 `
 
 	zshInsert = `
